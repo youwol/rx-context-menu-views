3cf9407ce4b142c71a6ab4a5a73fec19006dfd38
greinisch <greinisch@youwol.com>
2022-05-06 15:57:42 +0200

    ✨ context-menu persist until outside click

--- src/lib/context-menu.view.ts
+++ src/lib/context-menu.view.ts
@@ -1,55 +1,71 @@
-
 import { Observable, Subscription } from 'rxjs'
 import { map, tap } from 'rxjs/operators'
-import { render, VirtualDOM} from '@youwol/flux-view'
-
-
+import { render, VirtualDOM } from '@youwol/flux-view'
 
 export namespace ContextMenu {
+    export abstract class State {
+        content$: Observable<{ content: VirtualDOM; event: MouseEvent }>
 
-    export abstract class State{
-
-        
-        content$: Observable<{content:VirtualDOM, event: MouseEvent}>
-
-        constructor( public readonly triggerEvent$: Observable<MouseEvent>){
-
-            this.content$ = triggerEvent$.pipe( 
-                tap(ev => ev.preventDefault()), 
-                map( ev => {
-                    return {content: this.dispatch(ev), event:ev} 
-                })
+        constructor(public readonly triggerEvent$: Observable<MouseEvent>) {
+            this.content$ = triggerEvent$.pipe(
+                tap((ev) => ev.preventDefault()),
+                map((ev) => {
+                    return { content: this.dispatch(ev), event: ev }
+                }),
             )
         }
 
-        abstract dispatch(ev: MouseEvent) : VirtualDOM
+        abstract dispatch(ev: MouseEvent): VirtualDOM
     }
 
     export class View implements VirtualDOM {
-
         state: State
         subscriptions = new Array<Subscription>()
 
-        constructor(  {state, ...rest} : {state: State}) {
-            
-            Object.assign(this, rest)
+        constructor({
+            state,
+            zIndex,
+            ...rest
+        }: {
+            state: State
+            zIndex?: number
+            [_key: string]: any
+        }) {
             this.state = state
             this.subscriptions.push(
-                this.state.content$.subscribe( ({content, event}) => {
-                    
+                this.state.content$.subscribe(({ content, event }) => {
                     content.style = {
-                        position:'absolute', 
-                        left: `${event.clientX - 10 }px`, 
-                        top: `${event.clientY - 10}px`
+                        position: 'absolute',
+                        left: `${event.clientX - 10}px`,
+                        top: `${event.clientY - 10}px`,
+                        ...(rest.style || {}),
                     }
 
-                    let div = render(content)
-                    
-                    document.body.appendChild(div)
-                    div.onmouseleave = () => {
-                        div.remove()
+                    let wrapped = {
+                        style: {
+                            position: 'fixed',
+                            top: '0px',
+                            left: '0px',
+                            width: '100vw',
+                            height: '100vh',
+                            zIndex: zIndex || 10,
+                        },
+                        children: [content],
+                        onclick: () => {
+                            div.remove()
+                        },
+                        oncontextmenu: (ev: MouseEvent) => {
+                            div.remove()
+                            ev.preventDefault()
+                        },
                     }
-                })
+                    let div = render(wrapped)
+
+                    document.body.appendChild(div)
+                    /*div.onmouseleave = () => {
+                        div.remove()
+                    }*/
+                }),
             )
         }
     }
