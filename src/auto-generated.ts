efe3cfd42a685642b25381c12b21b2051396665d
youwol-guillaume <greinisch@youwol.com>
2022-09-14 06:46:28 +0200

    :bookmark: 0.1.1 - pipeline updated

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -1,31 +1,56 @@
+
 const runTimeDependencies = {
-    load: {
-        rxjs: '^6.5.5',
-        '@youwol/flux-view': '^1.0.0',
+    "load": {
+        "rxjs": "^6.5.5",
+        "@youwol/flux-view": "^1.0.3"
     },
-    differed: {},
-    includedInBundle: [],
+    "differed": {},
+    "includedInBundle": []
 }
 const externals = {
-    rxjs: 'rxjs_APIv6',
-    '@youwol/flux-view': '@youwol/flux-view_APIv1',
-    'rxjs/operators': {
-        commonjs: 'rxjs/operators',
-        commonjs2: 'rxjs/operators',
-        root: ['rxjs_APIv6', 'operators'],
+    "rxjs": {
+        "commonjs": "rxjs",
+        "commonjs2": "rxjs",
+        "root": "rxjs_APIv6"
     },
+    "@youwol/flux-view": {
+        "commonjs": "@youwol/flux-view",
+        "commonjs2": "@youwol/flux-view",
+        "root": "@youwol/flux-view_APIv1"
+    },
+    "rxjs/operators": {
+        "commonjs": "rxjs/operators",
+        "commonjs2": "rxjs/operators",
+        "root": [
+            "rxjs_APIv6",
+            "operators"
+        ]
+    }
+}
+const exportedSymbols = {
+    "rxjs": {
+        "apiKey": "6",
+        "exportedSymbol": "rxjs"
+    },
+    "@youwol/flux-view": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/flux-view"
+    }
 }
 export const setup = {
-    name: '@youwol/fv-context-menu',
-    assetId: 'QHlvdXdvbC9mdi1jb250ZXh0LW1lbnU=',
-    version: '0.1.0',
-    shortDescription: 'Context-menu using flux-view.',
-    developerDocumentation:
-        'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/fv-context-menu',
-    npmPackage: 'https://www.npmjs.com/package/@youwol/fv-context-menu',
-    sourceGithub: 'https://github.com/youwol/fv-context-menu',
-    userGuide: 'https://l.youwol.com/doc/@youwol/fv-context-menu',
-    apiVersion: '01',
+    name:'@youwol/fv-context-menu',
+        assetId:'QHlvdXdvbC9mdi1jb250ZXh0LW1lbnU=',
+    version:'0.1.1',
+    shortDescription:"Context-menu using flux-view.",
+    developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/fv-context-menu',
+    npmPackage:'https://www.npmjs.com/package/@youwol/fv-context-menu',
+    sourceGithub:'https://github.com/youwol/fv-context-menu',
+    userGuide:'https://l.youwol.com/doc/@youwol/fv-context-menu',
+    apiVersion:'01',
     runTimeDependencies,
     externals,
+    exportedSymbols,
+    getDependencySymbolExported: (module:string) => {
+        return `${exportedSymbols[module].exportedSymbol}_APIv${exportedSymbols[module].apiKey}`
+    }
 }
